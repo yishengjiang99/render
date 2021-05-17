04d0c6cb27432cf857f9c9fd61bf4ad0d732c864
Yisheng Jiang <yishengjiang@yahoo.com>
2021-05-17 13:50:15 -0700

    glorified ring buff

--- delay.c
+++ delay.c
@@ -1,39 +1,44 @@
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
-#define delay_buffer_size 4096
-#define delay_input_size 128
-#define write_index_mask 0x0000FFFUL
 
 typedef unsigned int uint32_t;
-typedef unsigned short uint16_t;
 typedef struct _delay
 {
+	uint16_t size;
+	float *buffer;
+	float *end;
+	float *read;
+	float *write;
 
-	uint32_t buffer_size;
-	uint32_t input_size;
-	int32_t write_offset;
-	float *delaybuffer;
+} delay_t;
 
-} delay_data;
-
-delay_data *newDelay()
+delay_t *newDelay(uint16_t size, uint16_t delay)
 {
-	delay_data *dl = (delay_data *)malloc(sizeof(delay_data));
-	dl->buffer_size = delay_buffer_size;
-	dl->input_size = delay_input_size;
-	dl->write_offset = 0;
-	dl->delaybuffer = (float *)malloc(sizeof(float) * delay_buffer_size);
-	return dl;
+	delay_t *t = (delay_t *)malloc(sizeof(delay_t));
+	t->size = size;
+	t->buffer = (float *)malloc(size * sizeof(float));
+	t->end = t->buffer + size;
+	t->read = t->buffer;
+	t->write = t->buffer + delay;
+	return t;
 }
 
-void advance_write_ptr(delay_data *dl)
+void dl_write(delay_t *t, int n, float *fl)
 {
-	dl->write_offset = dl->write_offset + (dl->input_size << 1L);
-}
-float *write_ptr(delay_data *delay)
-{
-	return delay->delaybuffer + (delay->write_offset & write_index_mask);
-}
-float *read_ptr(delay_data *delay)
-{
-	return delay->delaybuffer + (((delay->write_offset - delay_buffer_size) << 1) & write_index_mask);
+	while (n-- > 0)
+	{
+
+		if (t->write == t->end)
+		{
+			t->write = t->buffer;
+		}
+		*t->write++ = *fl++;
+
+		t->read++;
+		if (t->read == t->end)
+		{
+			t->read = t->buffer;
+		}
+	}
 }
